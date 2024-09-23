1a1b8fb3bf71dacd2b699d84cd7f01c77b1aa569
Artandrey <artandrey7777@gmail.com>
2024-09-23 15:38:02 +0300

    chore: remove unused dependencies from eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin', 'unused-imports'],
+  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: ['plugin:@typescript-eslint/recommended'],
   root: true,
   env: {
@@ -18,6 +18,5 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'unused-imports/no-unused-imports': 'error',
   },
 };
