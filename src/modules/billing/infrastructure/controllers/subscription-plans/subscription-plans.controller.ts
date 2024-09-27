a27e32d2273dc4d77520a9a7b39ac37af7dd637b
Artandrey <artandrey7777@gmail.com>
2024-09-27 12:27:10 +0300

    chore(stripe-billing): use abstraction for list subscriptions use case in subscription plans controller

--- src/modules/billing/infrastructure/controllers/subscription-plans/subscription-plans.controller.ts
+++ src/modules/billing/infrastructure/controllers/subscription-plans/subscription-plans.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
-import { ListSubscriptionPlansUseCase } from '~modules/billing/application/use-cases/list-subscription-plans/list-subscription-plans.use-case';
+import { IListSubscriptionPlansUseCase } from '~modules/billing/application/use-cases/list-subscription-plans/list-subscription-plans-use-case.interface';
+
+import { BillingDiToken } from '../../stripe/constants';
 
 @ApiTags('Subscription plans')
 @Controller('subscription/plans')
 export class SubscriptionPlansController {
-  constructor(private readonly listSubscriptionPlansUseCase: ListSubscriptionPlansUseCase) {}
+  constructor(
+    @Inject(BillingDiToken.LIST_SUBSCRIPTION_PLANS_USE_CASE)
+    private readonly listSubscriptionPlansUseCase: IListSubscriptionPlansUseCase,
+  ) {}
 
   @Get()
   public listSubscriptionPlans() {
