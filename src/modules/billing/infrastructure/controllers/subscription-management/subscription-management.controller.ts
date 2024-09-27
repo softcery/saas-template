1791e5484d0e56f4d174527f8c15fa57005bdef2
Artandrey <artandrey7777@gmail.com>
2024-09-27 12:24:54 +0300

    feat(stripe-billing): implement subscription management controller

--- src/modules/billing/infrastructure/controllers/subscription-management/subscription-management.controller.ts
+++ src/modules/billing/infrastructure/controllers/subscription-management/subscription-management.controller.ts
@@ -1,45 +1,50 @@
-// import { Body, Controller, Post } from '@nestjs/common';
-// import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
-// import { UserId } from '~modules/auth/application/decorators/user-id/user-id.decorator';
-// import { ArrangeSubscriptionDto } from '~modules/billing/application/dto/arrange-subscription.dto';
-// import { ArrangeSubscriptionUseCase } from '~modules/billing/application/use-cases/arrange-subscription/arrange-subscription.use-case';
-// import { CancelSubscriptionUseCase } from '~modules/billing/application/use-cases/cancel-subscription/cancel-subscription.use-case';
-// import { UpgradeSubscriptionUseCase } from '~modules/billing/application/use-cases/upgrade-subscription/upgrade-subscription.use-case';
+import { UserId } from '~modules/auth/infrastructure/decorators/user-id/user-id.decorator';
+import { ArrangeSubscriptionDto } from '~modules/billing/application/dto/arrange-subscription.dto';
+import { IArrangeSubscriptionUseCase } from '~modules/billing/application/use-cases/arrange-subscription/arrange-subscription-use-case.interface';
+import { ICancelSubscriptionUseCase } from '~modules/billing/application/use-cases/cancel-subscription/cancel-subscription-use-case.interface';
+import { IUpgradeSubscriptionUseCase } from '~modules/billing/application/use-cases/upgrade-subscription/upgrade-subscription-use-case.interface';
 
-// @ApiTags('Subscription plans')
-// @Controller('subscription/management')
-// export class SubscriptionManagementController {
-//   constructor(
-//     private readonly arrangeSubscriptionUseCase: ArrangeSubscriptionUseCase,
-//     private readonly upgradeSubscriptionUseCase: UpgradeSubscriptionUseCase,
-//     private readonly cancelSubscriptionUseCse: CancelSubscriptionUseCase,
-//   ) {}
+import { BillingDiToken } from '../../stripe/constants';
 
-//   @ApiOperation({
-//     operationId: 'arrangeSubscription',
-//   })
-//   @Post('arrange')
-//   public async arrangeSubscription(@UserId() userId: string, @Body() arrangeSubscriptionDto: ArrangeSubscriptionDto) {
-//     const result = await this.arrangeSubscriptionUseCase.execute(userId, arrangeSubscriptionDto);
-//     return result;
-//   }
+@ApiTags('Subscription plans')
+@Controller('subscription/management')
+export class SubscriptionManagementController {
+  constructor(
+    @Inject(BillingDiToken.ARRANGE_SUBSCRIPTION_USE_CASE)
+    private readonly arrangeSubscriptionUseCase: IArrangeSubscriptionUseCase,
+    @Inject(BillingDiToken.UPGRADE_SUBSCRIPTION_USE_CASE)
+    private readonly upgradeSubscriptionUseCase: IUpgradeSubscriptionUseCase,
+    @Inject(BillingDiToken.CANCEL_SUBSCRIPTION_USE_CASE)
+    private readonly cancelSubscriptionUseCse: ICancelSubscriptionUseCase,
+  ) {}
 
-//   @ApiOperation({
-//     operationId: 'upgradeSubscription',
-//   })
-//   @Post('upgrade')
-//   public async upgradeSubscription(@UserId() userId: string) {
-//     const result = await this.upgradeSubscriptionUseCase.execute(userId);
-//     return result;
-//   }
+  @ApiOperation({
+    operationId: 'arrangeSubscription',
+  })
+  @Post('arrange')
+  public async arrangeSubscription(@UserId() userId: string, @Body() arrangeSubscriptionDto: ArrangeSubscriptionDto) {
+    const result = await this.arrangeSubscriptionUseCase.execute({ userId, arrangeSubscriptionDto });
+    return result;
+  }
 
-//   @ApiOperation({
-//     operationId: 'cancelSubscription',
-//   })
-//   @Post('cancel')
-//   public async cancelSubscription(@UserId() userId: string) {
-//     const result = await this.cancelSubscriptionUseCse.execute(userId);
-//     return result;
-//   }
-// }
+  @ApiOperation({
+    operationId: 'upgradeSubscription',
+  })
+  @Post('upgrade')
+  public async upgradeSubscription(@UserId() userId: string) {
+    const result = await this.upgradeSubscriptionUseCase.execute({ userId });
+    return result;
+  }
+
+  @ApiOperation({
+    operationId: 'cancelSubscription',
+  })
+  @Post('cancel')
+  public async cancelSubscription(@UserId() userId: string) {
+    const result = await this.cancelSubscriptionUseCse.execute({ userId });
+    return result;
+  }
+}
