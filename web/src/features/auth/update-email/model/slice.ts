6505f4eaf2dc7972ee6f31b3a4c16cb42111b9cd
Artandrey <artandrey7777@gmail.com>
2024-10-01 16:30:49 +0300

    feat(web-auth): implement headless email update

---
+++ web/src/features/auth/update-email/model/slice.ts
@@ -0,0 +1,24 @@
+import { createApi, fakeBaseQuery } from '@reduxjs/toolkit/query/react'
+import { apiService } from '~/shared/api'
+import { UpdateUserEmailPayload } from '../types'
+
+export const changeEmailApi = createApi({
+  reducerPath: 'changeEmailApi',
+  baseQuery: fakeBaseQuery(),
+  endpoints: (build) => ({
+    changeEmailApi: build.mutation<void, UpdateUserEmailPayload>({
+      queryFn: async (payload: UpdateUserEmailPayload) => {
+        try {
+          await apiService().auth.changeEmail({ requestBody: payload })
+          return { data: undefined }
+        } catch (error) {
+          if (error instanceof Error) {
+            return { error: new Error(error['message']) }
+          } else {
+            return { error: new Error('Unknown error') }
+          }
+        }
+      },
+    }),
+  }),
+})
---
+++ web/src/features/auth/update-email/types.ts
@@ -0,0 +1,1 @@
+export type { UpdateUserEmailDto as UpdateUserEmailPayload } from '@shared/api-client'
