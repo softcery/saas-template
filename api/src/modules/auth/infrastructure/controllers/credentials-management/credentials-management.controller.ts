1d6c75ac0c185500a3bfc0c15c84e903a163c3ac
Artandrey <artandrey7777@gmail.com>
2024-10-01 13:03:54 +0300

    chore(auth): add open-api annotations to auth controllers

--- api/src/modules/auth/infrastructure/controllers/credentials-management/credentials-management.controller.ts
+++ api/src/modules/auth/infrastructure/controllers/credentials-management/credentials-management.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { ResetPasswordDto } from '~modules/auth/application/dto/reset-password.dto';
 import { SendResetPasswordConfirmationDto } from '~modules/auth/application/dto/send-reset-password-confirmation.dto';
@@ -14,6 +15,7 @@ import { User } from '~modules/auth/domain/entities/user.entity';
 import { UserId } from '../../decorators/user-id/user-id.decorator';
 import { ReqUser } from '../../decorators/user/user.decorator';
 
+@ApiTags('auth')
 @Controller('auth/credentials')
 export class CredentialsManagementController {
   constructor(
@@ -24,21 +26,25 @@ export class CredentialsManagementController {
     @Inject(AuthDiToken.RESET_PASSWORD_USE_CASE) private readonly resetPasswordUseCase: IResetPasswordUseCase,
   ) {}
 
+  @ApiOperation({ operationId: 'changePassword' })
   @Post('/password')
   public async changePassword(@Body() dto: UpdateUserPasswordDto, @UserId() userId: string) {
     return this.changePasswordUseCase.execute({ updateDto: dto, userId });
   }
 
+  @ApiOperation({ operationId: 'changeEmail' })
   @Post('/email')
   public async changeEmail(@Body() dto: UpdateUserEmailDto) {
     return this.changeEmailUseCase.execute(dto);
   }
 
+  @ApiOperation({ operationId: 'sendPasswordResetConfirmation' })
   @Post('/password/send-reset-confirmation')
   public async sendPasswordResetConfirmation(@Body() dto: SendResetPasswordConfirmationDto) {
     return this.sendResetPasswordConfirmationUseCase.execute(dto);
   }
 
+  @ApiOperation({ operationId: 'resetPassword' })
   @Post('/password/reset')
   public async resetPassword(@Body() dto: ResetPasswordDto, @ReqUser() user: User) {
     return this.resetPasswordUseCase.execute({
--- api/src/modules/auth/infrastructure/controllers/jwt-management/jwt-management.controller.ts
+++ api/src/modules/auth/infrastructure/controllers/jwt-management/jwt-management.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Post, UseGuards } from '@nestjs/common';
-import { ApiBody } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { ProvideRefreshTokenDto } from '~modules/auth/application/dto/provide-refresh-token.dto';
 
 import { JwtRefreshAuthGuard } from '../../supabase/guards/jwt-refresh-auth/jwt-refresh-auth.guard';
 
+@ApiTags('auth')
 @Controller('auth/tokens')
 export class JwtManagementController {
+  @ApiOperation({ operationId: 'refresh' })
   @ApiBody({
     type: ProvideRefreshTokenDto,
   })
