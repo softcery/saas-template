0be8c5987869b92f9ebe0acaadeb311ed3f25030
Artandrey <artandrey7777@gmail.com>
2024-10-02 21:31:25 +0300

    fix(web): resolve imports

--- web/src/app/index.tsx
+++ web/src/app/index.tsx
@@ -2,8 +2,6 @@ import { Router } from './providers/with-router'
 import { withProviders } from './providers'
 import { useAppInit } from './lib'
 
-import 'animate.css'
-
 const App = () => {
   useAppInit()
 
