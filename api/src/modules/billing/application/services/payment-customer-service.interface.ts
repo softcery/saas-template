e119003f211092b6fd711a0d13f9bce879e2e603
Artandrey <artandrey7777@gmail.com>
2024-10-02 14:05:11 +0300

    feat(stripe-billing): create payment customer on user creation

--- api/src/modules/billing/application/services/payment-customer-service.interface.ts
+++ api/src/modules/billing/application/services/payment-customer-service.interface.ts
@@ -1,13 +1,11 @@
 import { Customer } from '../../infrastructure/stripe/models/customer.model';
 
 export interface IPaymentCustomerCreationOptions {
-  name: string;
-  email: string;
+  name?: string;
+  email?: string;
 }
 
 export interface IPaymentCustomerService {
   createCustomer(customerOptions: IPaymentCustomerCreationOptions): Promise<Customer>;
-  // getCustomerByUserId(userId: string): Promise<PaymentCustomer>;
   getById(id: string): Promise<Customer>;
-  // saveCustomer(customer: PaymentCustomer): Promise<PaymentCustomer>;
 }
